6d49444737239e79d5c7075558c261567f3e978c
Carlos del Prado <carlos.delprado@11paths.com>
2021-07-22 22:00:32 +0200

    feat: contacts list & items

---
+++ src/components/contacts/contact-list/ContactList.js
@@ -0,0 +1,52 @@
+import { Component } from 'react';
+import ContactItem from '../contact-item/ContactItem';
+import contactsData from '../../../data/contacts.json';
+
+class ContactList extends Component {
+
+  state = {
+    contacts: []
+  }
+
+  componentDidMount() {
+    this.setState({ contacts: contactsData })
+  }
+
+  handleDeleteContact(id) {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id)
+    }))
+  }
+
+  handleResetContacts() {
+    this.setState({ contacts: contactsData })
+  }
+
+  render() {
+    const { contacts } = this.state;
+    return (
+      contacts &&
+        <>
+          <div className="row mb-2">
+            <div className="col">
+              <ul className="list-group">
+                {contacts.map(contact =>
+                  <li key={contact.id} className="list-group-item list-group-item-action">
+                    <ContactItem {...contact} onDeleteContact={(id) => this.handleDeleteContact(id)} />
+                  </li>
+                )}
+              </ul>
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-12 col-md-4">
+              <button className="btn btn-primary w-100" onClick={() => this.handleResetContacts()}>restart contacts</button>
+            </div>
+          </div>
+        </>
+    );
+  }
+
+}
+
+export default ContactList;
